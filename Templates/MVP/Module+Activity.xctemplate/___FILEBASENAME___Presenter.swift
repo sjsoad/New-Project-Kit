8fec757f550c9cf9edfb6546d2d0a089aa9eddb2
Sergey <sjsoad666@yandex.ru>
2018-09-04 14:19:41 +0300

    no message

---
+++ Templates/MVP/Module+Activity.xctemplate/___FILEBASENAME___Presenter.swift
@@ -0,0 +1,59 @@
+//
+//  ___FILENAME___
+//  ___PROJECTNAME___
+//
+//  Created by ___FULLUSERNAME___ on ___DATE___.
+//  ___COPYRIGHT___
+//
+
+import Foundation
+import SKActivityViewable
+import SKAlertViewable
+import SKNetworking
+
+protocol ___VARIABLE_fileName:identifier___Interface: class, ActivityViewable, AlertViewable {
+    
+}
+
+protocol ___VARIABLE_fileName:identifier___Output {
+    
+}
+
+class ___FILEBASENAMEASIDENTIFIER___: NSObject {
+    
+    private weak var view: ___VARIABLE_fileName:identifier___Interface?
+    
+    private var servicesRepository: ServicesRepository
+    
+    init(with view: ___VARIABLE_fileName:identifier___Interface, servicesRepository: ServicesRepository) {
+        self.view = view
+        self.servicesRepository = servicesRepository
+    }
+    
+}
+
+// MARK: - ___VARIABLE_fileName:identifier___Output -
+
+extension ___FILEBASENAMEASIDENTIFIER___: ___VARIABLE_fileName:identifier___Output {
+    
+}
+
+// MARK: - RequestExecuting -
+
+extension ___FILEBASENAMEASIDENTIFIER___: RequestExecuting {
+    
+    var activityView: ActivityViewable? {
+        return view
+    }
+    
+}
+
+// MARK: - RequestErrorHandling -
+
+extension ___FILEBASENAMEASIDENTIFIER___: RequestErrorHandling {
+    
+    var alertView: AlertViewable? {
+        return view
+    }
+    
+}
---
+++ Templates/MVP/Module.xctemplate/___FILEBASENAME___ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ___FILENAME___
+//  ___PROJECTNAME___
+//
+//  Created by ___FULLUSERNAME___ on ___DATE___.
+//  ___COPYRIGHT___
+//
+
+import UIKit
+
+class ___FILEBASENAMEASIDENTIFIER___: UIViewController, ___VARIABLE_fileName:identifier___Interface {
+
+    var presenter: ___VARIABLE_fileName:identifier___Output?
+
+    // MARK: - Lifecycle -
+    
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+    }
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+
+    // MARK: - ___VARIABLE_fileName:identifier___Interface -
+    
+}
